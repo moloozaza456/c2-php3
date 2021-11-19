d632f5fbe064d7ea8b595dad8d93a550a6e8a757
Dumrong <dumdumrong13@gmail.com>
2021-11-19 10:37:21 +0700

    Update v1

--- php301.php
+++ php301.php
@@ -1,3 +1,13 @@
 <?php
-echo "Welcome to My site";
+	$x = 10;
+	$y = 5;
+
+	echo  "PHP Arithmetic Operators";
+	echo "<h1>x = $x and y = $y","<hr color = 'red' siz='4' />";
+	echo "x + y = ",$x + $y,"<br />";
+	echo "x - y = ",$x - $y,"<br />";
+	echo "x * y = ",$x * $y,"<br />";
+	echo "x / y = ",$x / $y,"<br />";
+	echo "x ** y = ",$x ** $y,"<br />";
+	echo "x % y = ",$x % $y,"<br />";
 ?>
--- php302.php
+++ php302.php
@@ -1,5 +1,15 @@
 <?php
+	$x = 120;
+	
 
-echo "Operator";
+	echo "<h1>PHP Assignment Operator</h1>";
+	echo "<h1>x = ", $x ,"<br /></h1>";
+	echo "<hr color='red' size='4px'>";
+
+	echo "x += : ",$x += 100,"<br />"; //$x = $x + 100
+	echo "x -= : ",$x -= 100,"<br />"; //$x = $x - 100
+	echo "x *= : ",$x *= 100,"<br />"; //$x = $x * 100
+	echo "x /= : ",$x /= 100,"<br />"; //$x = $x / 100
+	echo "x %= : ",$x %= 100,"<br />"; //$x = $x % 100
 
 ?>
